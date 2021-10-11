ab699ad46f9246c5f8cd20980f09d9c3fe83d69a
hasnashab <78135389+hasnashab@users.noreply.github.com>
2021-10-11 17:51:12 +0700

    Add project files.

---
+++ FirstConsoleApp_056/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp_056
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
